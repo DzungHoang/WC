2ce803893d704e902972477c1beef3d6015db48b
DzungHoang <dzunghoang.cdt@gmail.com>
2014-03-12 00:27:31 +0700

    test commit to github

--- WC/src/lod/entertainment/wc/HomeFragment.java
+++ WC/src/lod/entertainment/wc/HomeFragment.java
@@ -11,6 +11,9 @@ public class HomeFragment extends Fragment {
 	 * The fragment argument representing the section number for this fragment.
 	 */
 	public static final String ARG_SECTION_NUMBER = "section_number";
+	
+	
+	
 
 	public HomeFragment() {
 	}
